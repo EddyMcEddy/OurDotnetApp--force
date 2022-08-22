8eecf3991c7311a76747a7e9ec648cd083fecf0c
Eddy Huapaya <ehuap@yahoo.com>
2022-08-22 01:10:09 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OurDotnetApp__force
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to C#");
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# OurDotnetApp__force
